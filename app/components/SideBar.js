2d287d4cf8b9c476da7783399a013cf6b1173f0b
jonatasfreitasv <jonatasfreitasv@gmail.com>
2018-04-26 02:45:03 -0300

    Refactor all design in SideBar Tree

--- app/components/SideBar.js
+++ app/components/SideBar.js
@@ -11,25 +11,85 @@ import {Treebeard, decorators} from 'react-treebeard';
 
 import { Scrollbars } from 'react-custom-scrollbars';
 
-import { Tabs, notification, Button, Layout } from 'antd';
+import { Tabs, notification, Button, Layout, Icon } from 'antd';
 const { Header, Footer, Sider, Content } = Layout;
 
 import axios from 'axios';
 
-decorators.Header = ({style, node}) => {
-
-  const iconType = node.children ? 'database' : 'table';
-  const iconClass = `fa fa-${iconType}`;
-  const iconStyle = {marginRight: '5px'};
+decorators.Toggle = ({style, node}) => {
 
   return (
-    <div style={style.base}>
-      <div style={style.title}>
-        <i className={iconClass} style={iconStyle}/>
-        {node.name} <small>{node.engine}{node.type}</small>
-      </div>
+    <div style={{position: 'relative', display: 'inline-block', height: '13px', width: '20px', transform: 'rotateZ(0deg)', marginTop: '4px'}}>
+      <svg height="13px" width="13px" viewBox="0 0 60 140" style={{fill: '#bbb'}}>
+        <g>
+          <path d="m40.4,121.3c-0.8,0.8-1.8,1.2-2.9,1.2s-2.1-0.4-2.9-1.2c-1.6-1.6-1.6-4.2 0-5.8l51-51-51-51c-1.6-1.6-1.6-4.2 0-5.8 1.6-1.6 4.2-1.6 5.8,0l53.9,53.9c1.6,1.6 1.6,4.2 0,5.8l-53.9,53.9z"/>
+        </g>
+      </svg>
     </div>
   );
+
+};
+
+decorators.Header = ({style, node}) => {
+
+  const iconType = node.icon;
+  const iconStyle = {marginRight: '5px'};
+
+  if(iconType === 'appstore') {
+
+    return (
+      <div style={style.base}>
+        <div style={style.title}>
+
+          <Icon type={iconType} style={{fontSize: '17px', marginRight: '5px'}}/>
+
+          <b style={{fontSize: '17px'}}>{node.name}</b> <small>({node.total_childrens})</small>
+
+        </div>
+      </div>
+    );
+
+  }else if(iconType === 'database') {
+
+    return (
+      <div style={style.base}>
+        <div style={style.title}>
+
+          <Icon type={iconType} style={{fontSize: '14px', marginRight: '5px'}}/>
+
+          <b>{node.name}</b> <small>({node.total_childrens})</small>
+
+        </div>
+      </div>
+    );
+
+  } else if (iconType !== '-') {
+
+    return (
+      <div style={style.base}>
+        <div style={style.title}>
+
+          <Icon type={iconType} style={iconStyle}/>
+
+          <b style={{fontSize: '13px'}}>{node.name}</b> <small>({node.total_childrens}) - {node.engine}</small>
+
+        </div>
+      </div>
+    );
+
+  } else {
+
+    return (
+      <div style={style.base}>
+        <div style={style.title}>
+          <div style={{marginLeft: '20px', fontSize: '13px'}}>
+            <i>{node.name}</i> <small>{node.engine}{node.type}</small>
+          </div>
+        </div>
+      </div>
+    );
+
+  }
 
 };
 
@@ -43,7 +103,7 @@ export default class SideBar extends Component {
       data: {}
     };
 
-    this.getData()
+    this.getData();
 
     this.onToggle = this.onToggle.bind(this);
     this.getData = this.getData.bind(this);
@@ -80,10 +140,32 @@ export default class SideBar extends Component {
 
               const columns = await this.query(`SELECT * FROM system.columns WHERE database='${database.name}' AND table='${table.name}'`);
 
+              let icon = 'table';
+
+              switch (table.engine) {
+                case 'Distributed': icon = 'cloud-o';
+                      break;
+                case 'Kafka': icon = 'share-alt';
+                      break;
+                case 'MaterializedView': icon = 'eye-o';
+                      break;
+                case 'ReplicatedMergeTree': icon = 'copy';
+                      break;
+                case 'MergeTree': icon = 'profile';
+                      break;
+              }
+
               return {
+                icon,
                 name: table.name,
                 engine: table.engine,
-                children: columns.data.data
+                total_childrens: columns.data.data.length,
+                children: columns.data.data.map(value => ({
+                  icon: '-',
+                  type: `${value.type} (${parseInt(parseInt(value.data_compressed_bytes, 10)/1024, 10)} kb)`,
+                  name: value.name
+                }))
+
               }
 
             })
@@ -92,7 +174,9 @@ export default class SideBar extends Component {
 
           return {
             name: database.name,
-            children: t_tree
+            children: t_tree,
+            total_childrens: t_tree.length,
+            icon: 'database',
           }
 
         })
@@ -101,11 +185,14 @@ export default class SideBar extends Component {
 
       this.setState({
         data:{
+          icon: 'appstore',
           name: 'Databases',
           toggled: true,
-          children: db_tree
+          children: db_tree,
+          total_childrens: db_tree.length,
         }
-      })
+      });
+
 
     }
     catch(err){
@@ -137,7 +224,7 @@ export default class SideBar extends Component {
     return (
 
 
-        <Content style={{background: '#333'}}>
+        <Content style={{background: '#333', height: '100vh'}}>
 
           <Content style={{padding: '10px', marginTop: '-30px'}}>
             <Button style={{marginTop: '2vh', width: '100%'}} type="dashed" icon="reload" loading={this.state.loading}
@@ -145,12 +232,10 @@ export default class SideBar extends Component {
             </Button>
           </Content>
 
-          <Scrollbars style={{height: '90vh'}}>
-
-            <Content style={{padding:'10px'}}>
+          <Scrollbars style={{height: '80vh'}}>
+            <Content style={{padding: '10px'}}>
               <Treebeard data={this.state.data} decorators={decorators} onToggle={this.onToggle}/>
             </Content>
-
           </Scrollbars>
 
         </Content>
