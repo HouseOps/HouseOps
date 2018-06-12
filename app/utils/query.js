233be5ad919980d0783ab0d63dd4e393908de4e6
Jonatas Freitas <jonatasfreitasv@gmail.com>
2018-06-12 12:10:29 -0300

    [BUG] Fix user and password authetication

--- app/utils/query.js
+++ app/utils/query.js
@@ -1,23 +1,22 @@
 import axios from 'axios';
+import buildUrl from 'build-url';
 
 import localStorageVariables from './localStorageVariables';
 
 export function databaseEndpoint(withDatabase = false) {
-  let endpoint = localStorage.getItem(localStorageVariables.database.host);
+  const url = {
+    path: '/',
+    queryParams: {
+      user: localStorage.getItem(localStorageVariables.database.user),
+      password: localStorage.getItem(localStorageVariables.database.pass)
+    }
+  };
 
-  if (localStorage.getItem(localStorageVariables.database.user)) {
-    endpoint = `${endpoint}/?user=${localStorage.getItem(localStorageVariables.database.user)}`;
+  if (withDatabase) {
+    url.queryParams.database = localStorage.getItem(localStorageVariables.database.use);
   }
 
-  if (localStorage.getItem(localStorageVariables.database.pass)) {
-    endpoint = `${endpoint}&password=${localStorage.getItem(localStorageVariables.database.pass)}`;
-  }
-
-  if (withDatabase && localStorage.getItem(localStorageVariables.database.use)) {
-    endpoint += `?database=${localStorage.getItem(localStorageVariables.database.use)}`;
-  }
-
-  return endpoint;
+  return buildUrl(localStorage.getItem(localStorageVariables.database.host), url);
 }
 
 export async function runQuery(query, withDatabase = false) {
